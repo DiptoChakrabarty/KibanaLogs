d72ea9e4664832f6caa37c1aaab37d50aeff54ad
DiptoChakrabarty <diptochuck123@gmail.com>
2020-05-09 18:37:59 +0530

    starting app

--- logs.py
+++ logs.py
@@ -1,3 +1,17 @@
 import logging
-import random
+import random,os,sys
 import logstash
+
+
+logging.basicConfig(filename="app.log",
+                    filemode='a',
+                    format='%(asctime)s %(levelname)s-%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+for i in range(0,15):
+    x=random.randint(0,2)
+    if(x==0):
+        logging.warning('Log Message')
+    elif(x==1):
+        logging.critical('Log Message')
+    else:
+        logging.error('Log Message')
